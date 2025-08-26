54c0a52c9d6ecd21136de230f581462677e3fc5c
Tu Nombre <tu-correo@ejemplo.com>
2025-08-26 11:37:43 -0300

    chore(build): agregar build.mjs programático

---
+++ scripts/build.mjs
@@ -0,0 +1,10 @@
+﻿import { build } from "vite";
+
+try {
+  await build(); // usa tu vite.config.* automáticamente
+  console.log("✔ Vite build OK");
+} catch (err) {
+  console.error("❌ Vite build FAILED");
+  console.error(err);
+  process.exit(1);
+}
